844df5813c63a6055e884cf0e5341c3d5f0c6aa6
fadhilmhdrzk <fadhil2634@gmail.com>
2025-06-11 15:39:37 +0700

    First Branch fadhil

--- src/App.jsx
+++ src/App.jsx
@@ -2,55 +2,25 @@
 import { Routes, Route, Link } from 'react-router-dom';
 import AdminPage from './components/Admin/AdminPage';
 import { useState } from 'react';
+import GuestLayout from './layouts/Guest/GuestLayout';
+import Home from './pages/Guest/Home';
+import Profile from './pages/Guest/Profile';
 
 function App() {
   return (
     <div className="p-4">
-      {/* Navigation Bar */}
-      <nav className="mb-6 flex gap-4 bg-blue-100 p-4 rounded-lg">
-        <Link to="/" className="text-blue-600 hover:text-blue-800">
-          Home
-        </Link>
-        <Link to="/admin" className="text-blue-600 hover:text-blue-800">
-          Admin Dashboard
-        </Link>
-      </nav>
 
       {/* Routes */}
       <Routes>
-        <Route path="/" element={<HomePage />} />
         <Route path="/admin" element={<AdminPage />} />
+
+        <Route element={<GuestLayout />}>
+          <Route path="/" element={<Home/>} />
+          <Route path="/profile" element={<Profile/>} />
+        </Route>
       </Routes>
+
     </div>
-  );
-}
-
-function HomePage() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   );
 }
 
